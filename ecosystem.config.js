4598b1749d4be91eea689cc1f40dacfa234730a5
lcfme <lcfme@sina.com>
2018-10-18 22:30:57 +0800

    done

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [
+    {
+      name: '1024-ws-server',
+      script: './dist/server.js',
+      cwd: './dist',
+      // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    }
+  ]
+};
