8504be7c45a5d74d5ca8bfa722b1fa87be295710
lcfme <lcfme@sina.com>
2018-10-21 10:41:55 +0800

    done

--- src/exam-database/index.js
+++ src/exam-database/index.js
@@ -1,19 +1,3 @@
 export default () => {
-  return [
-    {
-      content: 'hello',
-      answer: 1,
-      options: ['aaa', 'bbb', 'ccc', 'ddd']
-    },
-    {
-      content: 'foo',
-      answer: 0,
-      options: ['aaa', 'bbb', 'ccc', 'ddd']
-    },
-    {
-      answer: 3,
-      content: 'bar',
-      options: ['aaa', 'bbb', 'ccc', 'ddd']
-    }
-  ];
+  return [{"answer":3,"content":"sausage","options":[".相称性的,均衡的","强化，增强","微弱的闪光","香肠,腊肠"]},{"answer":1,"content":"champagne","options":["手抄的","香槟酒, 香槟色","司令部,指挥部,总部","蹒跚,踌躇,摇摆不定"]},{"answer":0,"content":"heal","options":["痊愈,恢复,和解","精神,勇气,","猛烈的, 强烈的","走失的家畜,浪子"]},{"answer":0,"content":"balcony","options":["阳台；楼厅，楼座","收集,积聚","恶化,降低,退化","\u00A0气压计，睛雨表"]},{"answer":3,"content":"barometer","options":["恶化,降低,退化","厉行, 执行, 强制","收集,积聚","\u00A0气压计，睛雨表"];
 };
